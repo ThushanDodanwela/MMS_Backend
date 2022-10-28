0559633474a5f66f393400399aa031cc6acade82
LasithEranga <lasitheranga1@gmail.com>
2022-10-29 02:25:47 +0530

    feat: update lecturer

--- controllers/LecturerController.js
+++ controllers/LecturerController.js
@@ -1,3 +1,4 @@
+import mongoose from "mongoose";
 import Lecturer from "../models/Lecturer.js";
 
 export const getAllLecturers = async (req, res) => {
@@ -33,6 +34,37 @@ export const newLecturer = async (req, res) => {
   } catch (error) {
     res.status(500).json({
       message: "Error creating new lecturer",
+      error,
+    });
+  }
+};
+
+export const updateLecturer = async (req, res) => {
+  const { _id, name, position, email, phoneNumber, qualifications } = req.body;
+
+  try {
+    // find lecturer in the database
+    const lecturer = await Lecturer.findOneAndUpdate(
+      {
+        _id: _id,
+      },
+      {
+        name: name,
+        position: position,
+        email: email,
+        phoneNumber: phoneNumber,
+        qualifications: qualifications,
+      }
+    );
+    console.log(lecturer);
+    lecturer.save();
+    res.status(200).json({
+      message: "success",
+      lecturer,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "error updating lecturer",
       error,
     });
   }
--- routes/LecturerRoutes.js
+++ routes/LecturerRoutes.js
@@ -2,11 +2,13 @@ import express from "express";
 import {
   getAllLecturers,
   newLecturer,
+  updateLecturer,
 } from "../controllers/LecturerController.js";
 
 const router = express.Router();
 
 router.post("/get-all", getAllLecturers);
 router.post("/new-lecturer", newLecturer);
+router.post("/update-lecturer", updateLecturer);
 
 export default router;
