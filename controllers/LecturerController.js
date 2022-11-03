29965b0a230410b49c332deb7c0c6b1ebbf44344
LasithEranga <lasitheranga1@gmail.com>
2022-11-04 03:27:03 +0530

    feat: password reset

--- controllers/LecturerController.js
+++ controllers/LecturerController.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 import Lecturer from "../models/Lecturer.js";
-
+import nodemailer from "nodemailer";
 export const getAllLecturers = async (req, res, next) => {
   //TODO: remove password from the projection
   try {
@@ -90,24 +90,112 @@ export const updateLecturer = async (req, res, next) => {
   }
 };
 
+export const sendOTP = async (req, res, next) => {
+  const { email } = req.body;
+  // find lecturer in the database
+  const lecturer = await Lecturer.findOneAndUpdate({
+    email: email,
+  });
+
+  try {
+    if (lecturer) {
+      let OTP = (Math.random() * 100000).toFixed();
+      let transporter = nodemailer.createTransport({
+        service: "gmail",
+        auth: {
+          user: process.env.USER,
+          pass: process.env.PASSWORD,
+        },
+      });
+
+      let mailOptions = {
+        from: "mms.dim.kln@gmail.com",
+        to: "lasitheranga1@gmail.com", //TODO:replace email with lecturer email
+        subject: "MMS - Password Reset",
+        html: `<h1>Hi ${
+          lecturer.name.split(" ")[0]
+        },</h1><h3>The OTP is:</h3><span>${OTP}</span>`,
+      };
+
+      transporter.sendMail(mailOptions, function (error, info) {
+        if (error) {
+          res.status(500).json({
+            message: "Error sending OTP",
+            error,
+          });
+        } else {
+          lecturer.password = OTP;
+          lecturer.save();
+          res.status(200).json({
+            message: "success",
+            info: info.response,
+          });
+        }
+      });
+    } else {
+      res.status(500).json({
+        message: "Email does not exists",
+      });
+    }
+  } catch (error) {
+    res.status(500).json({
+      message: "Something went wrong",
+      error,
+    });
+    return next(error);
+  }
+};
+
+export const verifyOTP = async (req, res, next) => {
+  const { email, otp } = req.body;
+
+  try {
+    const lecturer = await Lecturer.findOne({
+      email: email,
+      password: otp,
+    });
+    if (lecturer) {
+      res.status(200).json({
+        message: "success",
+      });
+    } else {
+      res.status(500).json({
+        message: "Verification Failed",
+      });
+    }
+  } catch (error) {
+    res.status(500).json({
+      message: "Something went wrong",
+      error,
+    });
+    return next(error);
+  }
+};
+
 export const resetPassword = async (req, res, next) => {
-  const { email, password } = req.body;
+  const { email, password, otp } = req.body;
 
   try {
     // find lecturer in the database
     const lecturer = await Lecturer.findOneAndUpdate(
       {
         email: email,
+        password: otp,
       },
       {
         password: password,
       }
     );
-
-    lecturer.save();
-    res.status(200).json({
-      message: "success",
-    });
+    // lecturer.save();
+    if (lecturer) {
+      res.status(200).json({
+        message: "success",
+      });
+    } else {
+      res.status(200).json({
+        message: "Error resetting password",
+      });
+    }
   } catch (error) {
     res.status(500).json({
       message: "Error resetting password",
--- routes/LecturerRoutes.js
+++ routes/LecturerRoutes.js
@@ -5,12 +5,16 @@ import {
   login,
   newLecturer,
   resetPassword,
+  sendOTP,
   updateLecturer,
+  verifyOTP,
 } from "../controllers/LecturerController.js";
 
 const router = express.Router();
 
 router.post("/login", login);
+router.post("/send-otp", sendOTP);
+router.post("/verify-otp", verifyOTP);
 router.post("/reset-password", resetPassword);
 router.post("/get-all", getAllLecturers);
 router.post("/delete-all", deleteAllLecturers);
